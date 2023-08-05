dcdc6553e60dcc59576dc1e3d2a65ba69c0c79af
Yuan Zhang <zhangyuan@users.noreply.github.com>
2023-08-05 15:20:48 +0800

    enable cgo

--- build.sh
+++ build.sh
@@ -6,12 +6,12 @@ BINARY_NAME=${BINARY_NAME:-csv2sqlite3}
 
 rm -rf bin/${BINARY_NAME}*
 
-GOOS=windows GOARCH=amd64 go build -ldflags "-w" -o bin/${BINARY_NAME}-amd64.exe
+CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -ldflags "-w" -o bin/${BINARY_NAME}-amd64.exe
 
-GOOS=windows GOARCH=386 go build -ldflags "-w" -o bin/${BINARY_NAME}-386.exe
+CGO_ENABLED=1 GOOS=windows GOARCH=386 go build -ldflags "-w" -o bin/${BINARY_NAME}-386.exe
 
-GOOS=darwin GOARCH=amd64 go build -ldflags "-w" -o bin/${BINARY_NAME}-amd64-darwin
+CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -ldflags "-w" -o bin/${BINARY_NAME}-amd64-darwin
 
-GOOS=linux GOARCH=amd64 go build -ldflags "-w" -o bin/${BINARY_NAME}-amd64-linux
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -ldflags "-w" -o bin/${BINARY_NAME}-amd64-linux
 
-GOOS=linux GOARCH=386 go build -ldflags "-w" -o bin/${BINARY_NAME}-386-linux
+CGO_ENABLED=1 GOOS=linux GOARCH=386 go build -ldflags "-w" -o bin/${BINARY_NAME}-386-linux
